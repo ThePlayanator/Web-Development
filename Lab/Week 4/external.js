d10b4257cadb912cde722cba9546a5ee3fcac7dc
ThePlayanator <129831729+ThePlayanator@users.noreply.github.com>
2024-05-01 23:01:51 +0800

    Add files via upload

---
+++ Lab/Week 4/external.js
@@ -0,0 +1,8 @@
+<script type="text/JavaScript">
+// declare variable
+var title ="Faizal's Site";
+
+// Output statement
+document.write("<h1>" + title + "</h1>");
+document.write("<p>Welcome to my website.</p>");
+</script>
